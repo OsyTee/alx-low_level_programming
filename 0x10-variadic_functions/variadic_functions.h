ae9e5570c4b318ab00ec137c682680cf1ea2b23a
Toyin <toyincodetech@gmail.com>
2022-10-14 01:42:12 -0700

    Task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef __VARIADIC_H__
+#define __VARIADIC_H__
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -9,5 +9,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list);
+void print_int(va_list);
+void print_float(va_list);
+void print_string(va_list);
+
+/**
+ * struct format_t - entry signs
+ * @t: type of variables
+ * @f: function to print type of variable
+ */
+typedef struct format_t
+{
+char *t;
+void (*f)(va_list);
+}
+format_t;
 
 #endif 
