d60caf92317695f4aea9ab15e500c5a200a38edf
Toyin <toyincodetech@gmail.com>
2022-10-14 00:44:43 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,17 +7,18 @@
  * @...: Other parameters
  * Return: The sum of all parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
-unsigned int i = 0;
+unsigned int i;
 int sum = 0;
 
 if (n == 0)
 return (0);
 
 va_start(ap, n);
-for (; i < n; i++)
+for (i = 0; i < n; i++)
 sum += va_arg(ap, int);
 
 va_end(ap);
